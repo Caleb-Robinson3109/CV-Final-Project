1fd6456a99eb77f1cbd8deb58086b9e21dfdf144
caleb <calebrobinson3109@gmail.com>
2025-11-18 14:38:49 -0600

    added some files

---
+++ cs5404/dataset.py
@@ -0,0 +1,6 @@
+"""
+dataset.py
+This file will get the ground truths and the information created by the KITRO model and aggreated it into on dataset.
+It will also have a function that splits the data set randomlly into 5 sections so that we do not test on our training data.
+We will be ablt to test 5 different times because we are splitting the data into 5.
+"""
---
+++ cs5404/model.py
@@ -0,0 +1,6 @@
+"""
+model.py
+This file will take in a dataset and output the extimated parameters.
+The model that we will have in this file is a SVM, Random Forest, and Linear Regression.
+At the minimum on of these models will be created. Others will be created time permitting.
+"""
